add904c1b2a5100a40073e3137884b1105b0e140
deds <superman200129@gmail.com>
2020-08-12 00:11:59 +0300

    Added Readme

--- todo-list/src/main/java/ru/web/todolist/security/SecurityWebApplicationInitiallizer.java
+++
@@ -1,9 +0,0 @@
-package ru.web.todolist.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-import org.springframework.stereotype.Component;
-
-@Component
-public class SecurityWebApplicationInitiallizer extends AbstractSecurityWebApplicationInitializer {
-
-}
